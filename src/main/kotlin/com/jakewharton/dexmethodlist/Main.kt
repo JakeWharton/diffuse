e82de484fa06fcb665d2c879868491950da0afa5
Jake Wharton <jw@squareup.com>
2015-12-30 17:46:03 -0500

    Switch to dependency injection.

--- src/main/kotlin/com/jakewharton/dexmethodlist/Main.kt
+++ src/main/kotlin/com/jakewharton/dexmethodlist/Main.kt
@@ -4,6 +4,8 @@ import com.android.dex.Dex
 import com.android.dex.MethodId
 import java.io.ByteArrayInputStream
 import java.io.FileInputStream
+import java.io.InputStream
+import java.io.PrintStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import kotlin.collections.flatMap
@@ -15,6 +17,7 @@ import kotlin.collections.last
 import kotlin.collections.listOf
 import kotlin.collections.map
 import kotlin.collections.sorted
+import kotlin.collections.toList
 import kotlin.sequences.filter
 import kotlin.sequences.map
 import kotlin.sequences.toList
@@ -24,98 +27,107 @@ import kotlin.text.split
 import kotlin.text.startsWith
 import kotlin.text.substring
 
-class Main {
+
+class Main(
+    private val args: List<String>,
+    private val stdin: InputStream = System.`in`,
+    private val stdout: PrintStream = System.out
+) : Runnable {
+  private val dexMagic = byteArrayOf(0x64, 0x65, 0x78, 0x0a, 0x30, 0x33, 0x35, 0x00)
+
+  override fun run() {
+    args.map { FileInputStream(it) }
+        .defaultIfEmpty(stdin)
+        .map { it.readBytes() }
+        .flatMap {
+          if (it.startsWith(dexMagic)) {
+            listOf(it)
+          } else {
+            ZipInputStream(ByteArrayInputStream(it)).use { zis ->
+              zis.entries()
+                  .filter { it.name.endsWith(".dex") }
+                  .map { zis.readBytes() }
+                  .toList()
+            }
+          }
+        }
+        .map { Dex(it) }
+        .flatMap { dex ->
+          dex.methodIds()
+              .map { renderMethod(dex, it) }
+        }
+        .sorted()
+        .forEach { stdout.println(it) }
+  }
+
+  private fun renderMethod(dex: Dex, methodId: MethodId): String {
+    val type = humanName(dex.typeNames()[methodId.declaringClassIndex])
+    val method = dex.strings()[methodId.nameIndex]
+    val params = dex.readTypeList(dex.protoIds()[methodId.protoIndex].parametersOffset).types
+        .map { humanName(dex.typeNames()[it.toInt()], true) }
+        .joinToString(", ")
+    return "$type $method($params)"
+  }
+
+  private fun humanName(type: String, stripPackage: Boolean = false): String {
+    if (type.startsWith("[")) {
+      return humanName(type.substring(1), stripPackage) + "[]"
+    }
+    if (type.startsWith("L")) {
+      val name = type.substring(1, type.length - 1)
+      if (stripPackage) {
+        return name.split('/').last()
+      }
+      return name.replace('/', '.')
+    }
+    return when (type) {
+      "B" -> "byte"
+      "C" -> "char"
+      "D" -> "double"
+      "F" -> "float"
+      "I" -> "int"
+      "J" -> "long"
+      "S" -> "short"
+      "V" -> "void"
+      "Z" -> "boolean"
+      else -> throw IllegalArgumentException("Unknown type $type")
+    }
+  }
+
+  private fun <T> List<T>.defaultIfEmpty(value: T): List<T> {
+    return if (isNotEmpty()) this else listOf(value)
+  }
+
+  private fun ByteArray.startsWith(value: ByteArray): Boolean {
+    if (value.size > size) return false
+    value.forEachIndexed { i, byte ->
+      if (get(i) != byte) {
+        return false
+      }
+    }
+    return true
+  }
+
+  private fun ZipInputStream.entries(): Sequence<ZipEntry> {
+    return object : Sequence<ZipEntry> {
+      override fun iterator(): Iterator<ZipEntry> {
+        return object : Iterator<ZipEntry> {
+          var next: ZipEntry? = null
+
+          override fun hasNext(): Boolean {
+            next = nextEntry
+            return next != null
+          }
+
+          override fun next() = next!!
+        }
+      }
+    }
+  }
+
   companion object {
-    private val dexMagic = byteArrayOf(0x64, 0x65, 0x78, 0x0a, 0x30, 0x33, 0x35, 0x00)
-
-    @JvmStatic fun main(args: Array<String>) {
-      args.map { FileInputStream(it) }
-          .defaultIfEmpty(System.`in`)
-          .map { it.readBytes() }
-          .flatMap {
-            if (it.startsWith(dexMagic)) {
-              listOf(it)
-            } else {
-              ZipInputStream(ByteArrayInputStream(it)).use { zis ->
-                zis.entries()
-                    .filter { it.name.endsWith(".dex") }
-                    .map { zis.readBytes() }
-                    .toList()
-              }
-            }
-          }
-          .map { Dex(it) }
-          .flatMap { dex ->
-            dex.methodIds()
-                .map { renderMethod(dex, it) }
-          }
-          .sorted()
-          .forEach { println(it) }
-    }
-
-    private fun renderMethod(dex: Dex, methodId: MethodId): String {
-      val type = humanName(dex.typeNames()[methodId.declaringClassIndex])
-      val method = dex.strings()[methodId.nameIndex]
-      val params = dex.readTypeList(dex.protoIds()[methodId.protoIndex].parametersOffset).types
-          .map { humanName(dex.typeNames()[it.toInt()], true) }
-          .joinToString(", ")
-      return "$type $method($params)"
-    }
-
-    private fun humanName(type: String, stripPackage: Boolean = false): String {
-      if (type.startsWith("[")) {
-        return humanName(type.substring(1), stripPackage) + "[]"
-      }
-      if (type.startsWith("L")) {
-        val name = type.substring(1, type.length - 1)
-        if (stripPackage) {
-          return name.split('/').last()
-        }
-        return name.replace('/', '.')
-      }
-      return when (type) {
-        "B" -> "byte"
-        "C" -> "char"
-        "D" -> "double"
-        "F" -> "float"
-        "I" -> "int"
-        "J" -> "long"
-        "S" -> "short"
-        "V" -> "void"
-        "Z" -> "boolean"
-        else -> throw IllegalArgumentException("Unknown type $type")
-      }
-    }
-
-    private fun <T> List<T>.defaultIfEmpty(value: T): List<T> {
-      return if (isNotEmpty()) this else listOf(value)
-    }
-
-    private fun ByteArray.startsWith(value: ByteArray): Boolean {
-      if (value.size > size) return false
-      value.forEachIndexed { i, byte ->
-        if (get(i) != byte) {
-          return false
-        }
-      }
-      return true
-    }
-
-    private fun ZipInputStream.entries(): Sequence<ZipEntry> {
-      return object : Sequence<ZipEntry> {
-        override fun iterator(): Iterator<ZipEntry> {
-          return object : Iterator<ZipEntry> {
-            var next: ZipEntry? = null
-
-            override fun hasNext(): Boolean {
-              next = nextEntry
-              return next != null
-            }
-
-            override fun next() = next!!
-          }
-        }
-      }
+    @JvmStatic fun main(vararg args: String) {
+      Main(args.toList()).run()
     }
   }
 }
