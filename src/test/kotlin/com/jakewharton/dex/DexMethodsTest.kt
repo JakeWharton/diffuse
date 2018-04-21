2f4544c24a1decbc84aeb77aa1da38ffd95d2194
Jake Wharton <jakew@google.com>
2018-04-21 12:18:32 -0400

    Make D8 compiler the default, add DexParser.

--- src/test/kotlin/com/jakewharton/dex/DexMethodsTest.kt
+++ src/test/kotlin/com/jakewharton/dex/DexMethodsTest.kt
@@ -3,12 +3,29 @@ package com.jakewharton.dex
 import com.google.common.io.Resources
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameter
+import org.junit.runners.Parameterized.Parameters
 import java.io.File
 
+@RunWith(Parameterized::class)
 class DexMethodsTest {
+  companion object {
+    @JvmStatic
+    @Parameters(name = "{0}")
+    fun parameters() = listOf(arrayOf<Any>(false), arrayOf<Any>(true))
+  }
+
+  @Parameter @JvmField var legacyDxParam: Boolean? = null
+  private val legacyDx get() = legacyDxParam!!
+
   @Test fun types() {
     val types = File(Resources.getResource("types.dex").file)
-    val methods = dexMethods(types).map { it.render() }
+    val methods = DexParser.fromFiles(types)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Types <init>()",
         "Types test(String)",
@@ -29,7 +46,10 @@ class DexMethodsTest {
 
   @Test fun paramsJoined() {
     val params = File(Resources.getResource("params_joined.dex").file)
-    val methods = dexMethods(params).map { it.render() }
+    val methods = DexParser.fromFiles(params)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -38,7 +58,10 @@ class DexMethodsTest {
 
   @Test fun visibilities() {
     val visibilities = File(Resources.getResource("visibilities.dex").file)
-    val methods = dexMethods(visibilities).map { it.render() }
+    val methods = DexParser.fromFiles(visibilities)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Visibilities <init>()",
         "Visibilities test1()",
@@ -51,7 +74,10 @@ class DexMethodsTest {
   @Test fun multipleDexFiles() {
     val params = File(Resources.getResource("params_joined.dex").file)
     val visibilities = File(Resources.getResource("visibilities.dex").file)
-    val methods = dexMethods(params, visibilities).map { it.render() }
+    val methods = DexParser.fromFiles(params, visibilities)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -66,7 +92,10 @@ class DexMethodsTest {
 
   @Test fun apk() {
     val one = File(Resources.getResource("one.apk").file)
-    val methods = dexMethods(one).map { it.render() }
+    val methods = DexParser.fromFiles(one)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Types <init>()",
         "Types test(String)",
@@ -87,7 +116,10 @@ class DexMethodsTest {
 
   @Test fun apkMultipleDex() {
     val three = File(Resources.getResource("three.apk").file)
-    val methods = dexMethods(three).map { it.render() }
+    val methods = DexParser.fromFiles(three)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -117,7 +149,10 @@ class DexMethodsTest {
 
   @Test fun classFile() {
     val params = File(Resources.getResource("Params.class").file)
-    val methods = dexMethods(params).map { it.render() }
+    val methods = DexParser.fromFiles(params)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -127,7 +162,10 @@ class DexMethodsTest {
   @Test fun multipleClassFiles() {
     val params = File(Resources.getResource("Params.class").file)
     val visibilities = File(Resources.getResource("Visibilities.class").file)
-    val methods = dexMethods(params, visibilities).map { it.render() }
+    val methods = DexParser.fromFiles(params, visibilities)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -141,7 +179,10 @@ class DexMethodsTest {
 
   @Test fun jarFile() {
     val params = File(Resources.getResource("params_joined.jar").file)
-    val methods = dexMethods(params).map { it.render() }
+    val methods = DexParser.fromFiles(params)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -151,7 +192,10 @@ class DexMethodsTest {
   @Test fun multipleJarFiles() {
     val params = File(Resources.getResource("params_joined.jar").file)
     val visibilities = File(Resources.getResource("visibilities.jar").file)
-    val methods = dexMethods(params, visibilities).map { it.render() }
+    val methods = DexParser.fromFiles(params, visibilities)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -165,7 +209,10 @@ class DexMethodsTest {
 
   @Test fun jarMultipleClasses() {
     val three = File(Resources.getResource("three.jar").file)
-    val methods = dexMethods(three).map { it.render() }
+    val methods = DexParser.fromFiles(three)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -193,7 +240,10 @@ class DexMethodsTest {
 
   @Test fun dexBytes() {
     val bytes = Resources.toByteArray(Resources.getResource("params_joined.dex"))
-    val methods = dexMethods(bytes).map { it.render() }
+    val methods = DexParser.fromBytes(bytes)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -202,7 +252,10 @@ class DexMethodsTest {
 
   @Test fun apkBytes() {
     val bytes = Resources.toByteArray(Resources.getResource("one.apk"))
-    val methods = dexMethods(bytes).map { it.render() }
+    val methods = DexParser.fromBytes(bytes)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Types <init>()",
         "Types test(String)",
@@ -223,7 +276,10 @@ class DexMethodsTest {
 
   @Test fun classBytes() {
     val bytes = Resources.toByteArray(Resources.getResource("Params.class"))
-    val methods = dexMethods(bytes).map { it.render() }
+    val methods = DexParser.fromBytes(bytes)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -232,7 +288,10 @@ class DexMethodsTest {
 
   @Test fun jarBytes() {
     val bytes = Resources.toByteArray(Resources.getResource("params_joined.jar"))
-    val methods = dexMethods(bytes).map { it.render() }
+    val methods = DexParser.fromBytes(bytes)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -241,7 +300,10 @@ class DexMethodsTest {
 
   @Test fun aarExtractsJars() {
     val three = File(Resources.getResource("three.aar").file)
-    val methods = dexMethods(three).map { it.render() }
+    val methods = DexParser.fromFiles(three)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Params <init>()",
         "Params test(String, String, String, String)",
@@ -269,7 +331,10 @@ class DexMethodsTest {
 
   @Test fun synthetics() {
     val types = File(Resources.getResource("synthetics.dex").file)
-    val methods = dexMethods(types).map { it.render() }
+    val methods = DexParser.fromFiles(types)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render() }
     assertThat(methods).containsExactly(
         "Synthetics <init>()",
         "Synthetics access$000(Synthetics) → int",
@@ -287,7 +352,10 @@ class DexMethodsTest {
   @Test fun syntheticsWithoutNumbers() {
     val types = File(Resources.getResource("synthetics.dex").file)
     val bytes = types.readBytes()
-    val methods = dexMethods(listOf(bytes)).map { it.render(true) }
+    val methods = DexParser.fromBytes(bytes)
+        .withLegacyDx(legacyDx)
+        .listMethods()
+        .map { it.render(true) }
     assertThat(methods).containsExactly(
         "Synthetics <init>()",
         "Synthetics access(Synthetics) → int",
