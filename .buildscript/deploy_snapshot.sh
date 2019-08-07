8cd58983014ff976133737236ec655edb957821b
Jake Wharton <jakew@google.com>
2019-08-06 22:07:11 -0400

    Fix deploy script for new JDK

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -6,7 +6,7 @@
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
 SLUG="JakeWharton/dex-method-list"
-JDK="oraclejdk8"
+JDK="openjdk8"
 BRANCH="master"
 
 set -e
