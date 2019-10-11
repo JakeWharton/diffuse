4605cbf05dc9576be2d94e535e2561d91a602af8
Jake Wharton <jakew@google.com>
2019-10-11 15:49:18 -0400

    Eagerly parse arsc

--- dex-members-list/src/main/kotlin/com/jakewharton/diffuse/Arsc.kt
+++ dex-members-list/src/main/kotlin/com/jakewharton/diffuse/Arsc.kt
@@ -5,22 +5,22 @@ import com.google.devrel.gmscore.tools.apk.arsc.BinaryResourceFile
 import com.google.devrel.gmscore.tools.apk.arsc.ResourceTableChunk
 import okio.ByteString
 
-class Arsc private constructor(private val bytes: ByteString) {
-  private val arscBlamer by lazy {
-    val resourceFile = BinaryResourceFile(bytes.toByteArray())
-    val chunk = resourceFile.chunks.single()
-    check(chunk is ResourceTableChunk) { "Root arsc chunk is not a resource table "}
-    ArscBlamer(chunk).apply { blame() }
-  }
-
-  val configs get() = arscBlamer.typeChunks.map { it.typeName }
-
-  // TODO map this to model object?
-  val entries get() = arscBlamer.resourceEntries.keySet().map { "@${it.typeName()}/${it.entryName()}" }
-
+class Arsc private constructor(
+  val configs: List<String>,
+  val entries: List<String>
+) {
   companion object {
     @JvmStatic
     @JvmName("create")
-    fun ByteString.toArsc() = Arsc(this)
+    fun ByteString.toArsc(): Arsc {
+      val resourceFile = BinaryResourceFile(toByteArray())
+      val chunk = resourceFile.chunks.single()
+      check(chunk is ResourceTableChunk) { "Root arsc chunk is not a resource table "}
+      val arscBlamer = ArscBlamer(chunk).apply { blame() }
+      val configs = arscBlamer.typeChunks.map { it.typeName }
+      // TODO map this to model object?
+      val entries = arscBlamer.resourceEntries.keySet().map { "@${it.typeName()}/${it.entryName()}" }
+      return Arsc(configs, entries)
+    }
   }
 }
