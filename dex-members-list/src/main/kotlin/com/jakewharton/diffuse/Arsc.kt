e5573b1999f7fe504706cc71162a5b9f86492254
Jake Wharton <jakew@google.com>
2019-10-13 20:43:54 -0400

    Model Arsc entries with an object

--- dex-members-list/src/main/kotlin/com/jakewharton/diffuse/Arsc.kt
+++ dex-members-list/src/main/kotlin/com/jakewharton/diffuse/Arsc.kt
@@ -1,14 +1,29 @@
 package com.jakewharton.diffuse
 
 import com.google.devrel.gmscore.tools.apk.arsc.ArscBlamer
+import com.google.devrel.gmscore.tools.apk.arsc.ArscBlamer.ResourceEntry
 import com.google.devrel.gmscore.tools.apk.arsc.BinaryResourceFile
 import com.google.devrel.gmscore.tools.apk.arsc.ResourceTableChunk
 
 class Arsc private constructor(
   val configs: List<String>,
-  val entries: List<String>
+  val entries: List<Entry>
 ) {
+  data class Entry(
+    val type: String,
+    val name: String
+  ) : Comparable<Entry> {
+    override fun compareTo(other: Entry) = comparable.compare(this, other)
+    override fun toString() = "$type/$name"
+
+    private companion object {
+      private val comparable = compareBy(Entry::type).thenBy(Entry::name)
+    }
+  }
+
   companion object {
+    private fun ResourceEntry.toEntry() = Entry(typeName(), entryName())
+
     @JvmStatic
     @JvmName("create")
     fun BinaryResourceFile.toArsc(): Arsc {
@@ -16,8 +31,7 @@ class Arsc private constructor(
       check(chunk is ResourceTableChunk) { "Root arsc chunk is not a resource table "}
       val arscBlamer = ArscBlamer(chunk).apply { blame() }
       val configs = arscBlamer.typeChunks.map { it.typeName }
-      // TODO map this to model object?
-      val entries = arscBlamer.resourceEntries.keySet().map { "@${it.typeName()}/${it.entryName()}" }
+      val entries = arscBlamer.resourceEntries.keySet().map { it.toEntry() }
       return Arsc(configs, entries)
     }
   }
