38f4003c0aa74fcffb456822f59e0b69b47b43e5
Jake Wharton <jakew@google.com>
2019-10-13 21:35:13 -0400

    Change Dex to be eagerly parsed

--- dex-members-list/src/main/kotlin/com/jakewharton/diffuse/Dex.kt
+++ dex-members-list/src/main/kotlin/com/jakewharton/diffuse/Dex.kt
@@ -1,27 +1,33 @@
 package com.jakewharton.diffuse
 
 import com.jakewharton.dex.DexMember
-import com.jakewharton.dex.MemberList
 import com.jakewharton.dex.toMemberList
 import com.jakewharton.diffuse.io.Input
 import okio.BufferedSource
-import okio.ByteString
 import com.android.dex.Dex as AndroidDex
 
-class Dex private constructor(private val bytes: ByteString) {
-  private val dex: AndroidDex by lazy { AndroidDex(bytes.toByteArray()) }
-  private val memberList: MemberList by lazy { dex.toMemberList() }
-
-  val strings: List<String> get() = dex.strings()
-  val types: List<String> get() = dex.typeNames()
-  val classes: List<String> by lazy { dex.classDefs().map { dex.typeNames()[it.typeIndex] }}
-  val members: List<DexMember> get() = memberList.all
-  val declaredMembers: List<DexMember> get() = memberList.declared
-  val referencedMembers: List<DexMember> get() = memberList.referenced
-
+class Dex private constructor(
+  val strings: List<String>,
+  val types: List<String>,
+  val classes: List<String>,
+  val members: List<DexMember>,
+  val declaredMembers: List<DexMember>,
+  val referencedMembers: List<DexMember>
+) {
   companion object {
     @JvmStatic
     @JvmName("parse")
-    fun Input.toDex() = Dex(source().use(BufferedSource::readByteString))
+    fun Input.toDex(): Dex {
+      val bytes = source().use(BufferedSource::readByteArray)
+
+      val dex = AndroidDex(bytes)
+      val memberList = dex.toMemberList()
+      val classes = dex.classDefs().map { dex.typeNames()[it.typeIndex] }
+
+      return Dex(
+          dex.strings(), dex.typeNames(), classes, memberList.all, memberList.declared,
+          memberList.referenced
+      )
+    }
   }
 }
