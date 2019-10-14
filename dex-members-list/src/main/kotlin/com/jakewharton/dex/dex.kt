1b888fa6fddb9212f694e1325d77ef0fee958fa8
Jake Wharton <jakew@google.com>
2019-10-13 22:03:54 -0400

    Migrate magic prefixes to place of only use

--- dex-members-list/src/main/kotlin/com/jakewharton/dex/dex.kt
+++ dex-members-list/src/main/kotlin/com/jakewharton/dex/dex.kt
@@ -12,6 +12,9 @@ import com.jakewharton.dex.DexParser.Desugaring
 import java.io.ByteArrayInputStream
 import java.util.zip.ZipInputStream
 
+private val CLASS_MAGIC = byteArrayOf(0xCA.toByte(), 0xFE.toByte(), 0xBA.toByte(), 0xBE.toByte())
+private val DEX_MAGIC = byteArrayOf(0x64, 0x65, 0x78, 0x0a, 0x30, 0x33, 0x35, 0x00)
+
 internal fun Iterable<ByteArray>.toDexes(desugaring: Desugaring): List<Dex> {
   val classes = mutableListOf<ByteArray>()
   val dexes = mutableListOf<ByteArray>()
