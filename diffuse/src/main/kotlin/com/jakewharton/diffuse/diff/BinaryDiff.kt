7b17fbf87aacf47be06b4d608aecf122a1548908
Jake Wharton <jakew@google.com>
2019-10-20 00:25:05 -0400

    Move diff factories to BinaryDiff statics

--- diffuse/src/main/kotlin/com/jakewharton/diffuse/diff/BinaryDiff.kt
+++ diffuse/src/main/kotlin/com/jakewharton/diffuse/diff/BinaryDiff.kt
@@ -1,7 +1,44 @@
 package com.jakewharton.diffuse.diff
 
+import com.jakewharton.diffuse.Aab
+import com.jakewharton.diffuse.Aar
+import com.jakewharton.diffuse.ApiMapping
+import com.jakewharton.diffuse.Apk
+import com.jakewharton.diffuse.Jar
 import com.jakewharton.diffuse.report.DiffReport
 
 interface BinaryDiff {
   fun toTextReport(): DiffReport
+
+  companion object {
+    @JvmStatic
+    fun ofApk(
+      oldApk: Apk,
+      oldMapping: ApiMapping = ApiMapping.EMPTY,
+      newApk: Apk,
+      newMapping: ApiMapping = ApiMapping.EMPTY
+    ): BinaryDiff = ApkDiff(oldApk, oldMapping, newApk, newMapping)
+
+    @JvmStatic
+    fun ofAab(
+      oldAab: Aab,
+      newAab: Aab
+    ): BinaryDiff = AabDiff(oldAab, newAab)
+
+    @JvmStatic
+    fun ofAar(
+      oldAar: Aar,
+      oldMapping: ApiMapping = ApiMapping.EMPTY,
+      newAar: Aar,
+      newMapping: ApiMapping = ApiMapping.EMPTY
+    ): BinaryDiff = AarDiff(oldAar, oldMapping, newAar, newMapping)
+
+    @JvmStatic
+    fun ofJar(
+      oldJar: Jar,
+      oldMapping: ApiMapping = ApiMapping.EMPTY,
+      newJar: Jar,
+      newMapping: ApiMapping = ApiMapping.EMPTY
+    ): BinaryDiff = JarDiff(oldJar, oldMapping, newJar, newMapping)
+  }
 }
--- diffuse/src/main/kotlin/com/jakewharton/diffuse/diffuse.kt
+++ diffuse/src/main/kotlin/com/jakewharton/diffuse/diffuse.kt
@@ -13,42 +13,12 @@ import com.jakewharton.diffuse.Aar.Companion.toAar
 import com.jakewharton.diffuse.ApiMapping.Companion.toApiMapping
 import com.jakewharton.diffuse.Apk.Companion.toApk
 import com.jakewharton.diffuse.Jar.Companion.toJar
-import com.jakewharton.diffuse.diff.AabDiff
-import com.jakewharton.diffuse.diff.AarDiff
-import com.jakewharton.diffuse.diff.ApkDiff
 import com.jakewharton.diffuse.diff.BinaryDiff
-import com.jakewharton.diffuse.diff.JarDiff
 import com.jakewharton.diffuse.io.Input.Companion.asInput
 
 fun main(vararg args: String) {
   DiffuseCommand().main(args.toList())
 }
-
-fun apkDiff(
-  oldApk: Apk,
-  oldMapping: ApiMapping = ApiMapping.EMPTY,
-  newApk: Apk,
-  newMapping: ApiMapping = ApiMapping.EMPTY
-): BinaryDiff = ApkDiff(oldApk, oldMapping, newApk, newMapping)
-
-fun aabDiff(
-  oldAab: Aab,
-  newAab: Aab
-): BinaryDiff = AabDiff(oldAab, newAab)
-
-fun aarDiff(
-  oldAar: Aar,
-  oldMapping: ApiMapping = ApiMapping.EMPTY,
-  newAar: Aar,
-  newMapping: ApiMapping = ApiMapping.EMPTY
-): BinaryDiff = AarDiff(oldAar, oldMapping, newAar, newMapping)
-
-fun jarDiff(
-  oldJar: Jar,
-  oldMapping: ApiMapping = ApiMapping.EMPTY,
-  newJar: Jar,
-  newMapping: ApiMapping = ApiMapping.EMPTY
-): BinaryDiff = JarDiff(oldJar, oldMapping, newJar, newMapping)
 
 private class DiffuseCommand : CliktCommand(name = "diffuse") {
   private val old by argument("OLD", help = "Old input file.")
@@ -80,16 +50,16 @@ private class DiffuseCommand : CliktCommand(name = "diffuse") {
     val newMapping = newMappingPath?.asInput()?.toApiMapping() ?: ApiMapping.EMPTY
     val diff = when (type) {
       Type.Apk -> {
-        apkDiff(oldInput.toApk(), oldMapping, newInput.toApk(), newMapping)
+        BinaryDiff.ofApk(oldInput.toApk(), oldMapping, newInput.toApk(), newMapping)
       }
       Type.Aab -> {
-        aabDiff(oldInput.toAab(), newInput.toAab())
+        BinaryDiff.ofAab(oldInput.toAab(), newInput.toAab())
       }
       Type.Aar -> {
-        aarDiff(oldInput.toAar(), oldMapping, newInput.toAar(), newMapping)
+        BinaryDiff.ofAar(oldInput.toAar(), oldMapping, newInput.toAar(), newMapping)
       }
       Type.Jar -> {
-        jarDiff(oldInput.toJar(), oldMapping, newInput.toJar(), newMapping)
+        BinaryDiff.ofJar(oldInput.toJar(), oldMapping, newInput.toJar(), newMapping)
       }
     }
     println(diff.toTextReport())
