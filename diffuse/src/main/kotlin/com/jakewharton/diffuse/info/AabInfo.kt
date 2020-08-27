6a0d9cbc89d3281b88da423e416b0650ddd885be
Jake Wharton <jw@squareup.com>
2020-08-26 22:19:54 -0400

    Aab info report

---
+++ diffuse/src/main/kotlin/com/jakewharton/diffuse/info/AabInfo.kt
@@ -0,0 +1,14 @@
+package com.jakewharton.diffuse.info
+
+import com.jakewharton.diffuse.Aab
+import com.jakewharton.diffuse.diff.BinaryDiff
+import com.jakewharton.diffuse.report.Report
+import com.jakewharton.diffuse.report.text.AabInfoTextReport
+
+class AabInfo(
+  private val aab: Aab,
+) : BinaryDiff {
+  override fun toTextReport(): Report {
+    return AabInfoTextReport(aab)
+  }
+}
---
+++ diffuse/src/main/kotlin/com/jakewharton/diffuse/report/text/AabInfoTextReport.kt
@@ -0,0 +1,58 @@
+package com.jakewharton.diffuse.report.text
+
+import com.jakewharton.diffuse.Aab
+import com.jakewharton.diffuse.ArchiveFile
+import com.jakewharton.diffuse.diffuseTable
+import com.jakewharton.diffuse.info.toSummaryTable
+import com.jakewharton.diffuse.report.Report
+
+internal class AabInfoTextReport(private val aab: Aab) : Report {
+  override fun write(appendable: Appendable) {
+    appendable.apply {
+      appendLine(aab.filename)
+      appendLine()
+      appendLine(
+        diffuseTable {
+          header {
+            row("MODULES")
+          }
+
+          row("base")
+
+          for (name in aab.featureModules.keys) {
+            row(name)
+          }
+        }.toString()
+      )
+
+      appendModule("base", aab.baseModule)
+
+      for ((name, module) in aab.featureModules) {
+        appendModule(name, module)
+      }
+    }
+  }
+
+  private fun Appendable.appendModule(
+    name: String,
+    module: Aab.Module,
+  ) {
+    appendLine()
+    appendLine()
+    appendLine("==============${"=".repeat(name.length)}")
+    appendLine("====   $name   ====")
+    appendLine("==============${"=".repeat(name.length)}")
+    appendLine()
+
+    appendLine(
+      module.files.toSummaryTable(
+        "AAB",
+        ArchiveFile.Type.AAB_TYPES,
+        skipIfEmptyTypes = setOf(ArchiveFile.Type.Native)
+      )
+    )
+    appendLine(module.dexes.toSummaryTable())
+  }
+
+  override fun toString() = buildString { write(this) }
+}
