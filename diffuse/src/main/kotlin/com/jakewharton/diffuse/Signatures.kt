916629ec6a5b080d1c5e55244c6813bb80c8e7d7
Jake Wharton <jw@squareup.com>
2020-09-01 20:47:25 -0400

    Stream binaries to apksig when parsing signatures
    This saves us from converting the entire binary into a byte array first.

--- diffuse/src/main/kotlin/com/jakewharton/diffuse/Signatures.kt
+++ diffuse/src/main/kotlin/com/jakewharton/diffuse/Signatures.kt
@@ -14,14 +14,14 @@ data class Signatures(
     @JvmStatic
     @JvmName("parse")
     fun Input.toSignatures(): Signatures {
-      // TODO should be able to make a DataSource from a FileChannel to avoid toByteArray here.
-      val dataSource = toByteArray().asByteBuffer().asDataSource()
-      val result = ApkVerifier.Builder(dataSource).build().verify()
-      return Signatures(
-        result.v1SchemeSigners.map { it.certificate.encoded.toByteString().sha1() }.sorted(),
-        result.v2SchemeSigners.map { it.certificate.encoded.toByteString().sha1() }.sorted(),
-        result.v3SchemeSigners.map { it.certificate.encoded.toByteString().sha1() }.sorted()
-      )
+      useDataSource { dataSource ->
+        val result = ApkVerifier.Builder(dataSource).build().verify()
+        return Signatures(
+          result.v1SchemeSigners.map { it.certificate.encoded.toByteString().sha1() }.sorted(),
+          result.v2SchemeSigners.map { it.certificate.encoded.toByteString().sha1() }.sorted(),
+          result.v3SchemeSigners.map { it.certificate.encoded.toByteString().sha1() }.sorted()
+        )
+      }
     }
   }
 }
