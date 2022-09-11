c16c3b52dd582e0fae6e37a98918061befb4bb91
Goooler <wangzongler@gmail.com>
2022-09-11 22:16:28 +0800

    Replace todos with std implementations

--- diffuse/src/main/kotlin/com/jakewharton/diffuse/extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.jakewharton.diffuse
-
-import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Path
-
-// TODO https://youtrack.jetbrains.com/issue/KT-18242
-internal fun Path.writeText(text: String, charset: Charset = Charsets.UTF_8) = Files.write(this, text.toByteArray(charset))
--- io/src/main/kotlin/com/jakewharton/diffuse/io/util.kt
+++ io/src/main/kotlin/com/jakewharton/diffuse/io/util.kt
@@ -2,9 +2,8 @@ package com.jakewharton.diffuse.io
 
 import java.io.InputStream
 import java.nio.charset.Charset
+import java.nio.file.FileSystem
 import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.OpenOption
 import java.nio.file.Path
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
@@ -12,15 +11,12 @@ import okio.Buffer
 import okio.ByteString
 import okio.ByteString.Companion.toByteString
 
-internal fun InputStream.asZip(charset: Charset = Charsets.UTF_8) = ZipInputStream(this, charset)
-internal fun InputStream.readByteString() = readBytes().toByteString()
+internal fun InputStream.asZip(charset: Charset = Charsets.UTF_8): ZipInputStream = ZipInputStream(this, charset)
+internal fun InputStream.readByteString(): ByteString = readBytes().toByteString()
 
-// TODO https://youtrack.jetbrains.com/issue/KT-18242
-internal fun Path.inputStream(vararg options: OpenOption): InputStream = Files.newInputStream(this, *options)
-internal val Path.exists get() = Files.exists(this)
-internal fun Path.asZipFileSystem(loader: ClassLoader? = null) = FileSystems.newFileSystem(this, loader)!!
+internal fun Path.asZipFileSystem(loader: ClassLoader? = null): FileSystem = FileSystems.newFileSystem(this, loader)
 
-internal fun ByteString.asInputStream() = Buffer().write(this).inputStream()
+internal fun ByteString.asInputStream(): InputStream = Buffer().write(this).inputStream()
 
 internal fun ZipInputStream.entries(): Sequence<ZipEntry> {
   return object : Sequence<ZipEntry> {
