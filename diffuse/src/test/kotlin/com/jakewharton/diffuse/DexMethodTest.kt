98ee2854b7854dd2526341c51ac676f6e9f91c18
Goooler <wangzongler@gmail.com>
2024-01-20 12:29:41 +0800

    Migrate to AssertK

--- diffuse/src/test/kotlin/com/jakewharton/diffuse/DexMethodTest.kt
+++ diffuse/src/test/kotlin/com/jakewharton/diffuse/DexMethodTest.kt
@@ -1,6 +1,9 @@
 package com.jakewharton.diffuse
 
-import com.google.common.truth.Truth.assertThat
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import com.jakewharton.diffuse.format.Method
 import com.jakewharton.diffuse.format.TypeDescriptor
 import org.junit.Test
