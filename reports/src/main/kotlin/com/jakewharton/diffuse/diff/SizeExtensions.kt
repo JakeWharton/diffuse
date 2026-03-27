142dc3faf9907fb5f937ef1a2ecfd770123a5778
Jake Wharton <jw@jakewharton.com>
2026-03-26 21:32:55 -0400

    Remove redundant extension

--- reports/src/main/kotlin/com/jakewharton/diffuse/diff/SizeExtensions.kt
+++ reports/src/main/kotlin/com/jakewharton/diffuse/diff/SizeExtensions.kt
@@ -6,7 +6,7 @@ import me.saket.bytesize.ByteSize
 import me.saket.bytesize.DecimalBitSize
 import me.saket.bytesize.DecimalByteSize
 
-/** TODO: https://github.com/saket/byte-size/pull/21 */
+/** TODO: https://github.com/saket/byte-size/issues/22 */
 internal val ByteSize.absoluteValue: ByteSize
   get() =
     when (this) {
@@ -14,12 +14,3 @@ internal val ByteSize.absoluteValue: ByteSize
       is BinaryByteSize -> BinaryByteSize(inWholeBytes.absoluteValue)
       is DecimalByteSize -> DecimalByteSize(inWholeBytes.absoluteValue)
     }
-
-/** TODO: https://github.com/saket/byte-size/pull/20 */
-@Suppress("NOTHING_TO_INLINE")
-internal inline operator fun ByteSize.unaryMinus(): ByteSize =
-  when (this) {
-    is DecimalBitSize -> DecimalBitSize(-inWholeBits)
-    is BinaryByteSize -> BinaryByteSize(-inWholeBytes)
-    is DecimalByteSize -> DecimalByteSize(-inWholeBytes)
-  }
